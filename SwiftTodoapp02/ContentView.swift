8a16a4125277117f99666fdc3e5f30d11ffad708
Springfruits <shunkainoue0515@gmail.com>
2024-03-04 12:28:34 +0900

    initialization

---
+++ SwiftTodoapp02/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftTodoapp02
+//
+//  Created by 井上俊果 on 2024/03/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftTodoapp02/SwiftTodoapp02App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTodoapp02App.swift
+//  SwiftTodoapp02
+//
+//  Created by 井上俊果 on 2024/03/04.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTodoapp02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
